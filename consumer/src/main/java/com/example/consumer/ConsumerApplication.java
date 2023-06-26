08d496de14e8f4fa300e238e685703b3945a9c96
Raissa Machado <rmachado@bol.com>
2023-06-26 18:24:26 +0200

    Small improvements

--- consumer/src/main/java/com/example/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -3,12 +3,10 @@ package com.example.consumer;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class ConsumerApplication {
 
     public static void main(String[] args) {
