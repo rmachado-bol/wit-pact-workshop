16e0dc7c607c7df3f219b340c5c4ac0e6f72ceb9
Raissa Machado <rmachado@bol.com>
2023-06-26 18:22:05 +0200

    Small improvements

--- consumer/src/main/java/com/example/consumer/DigitalLibraryController.java
+++ consumer/src/main/java/com/example/consumer/DigitalLibraryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DigitalLibraryController {
 
-    private BookServiceAdapter bookServiceAdapter;
+    private final BookServiceAdapter bookServiceAdapter;
 
     public DigitalLibraryController(BookServiceAdapter bookServiceAdapter) {
         this.bookServiceAdapter = bookServiceAdapter;
